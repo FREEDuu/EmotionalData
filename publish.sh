560640bfdc3b268fb6410c1b6c87fae192509763
FREEDuu <derosafrancesco429@gmail.com>
2025-01-13 10:09:24 +0100

    Create new post !! :D

---
+++ publish.sh
@@ -0,0 +1,37 @@
+
+rsync -av --delete "/home/francesco/Documents/Obsidian Vault/static" "/home/francesco/Desktop/PersonalBlog/"
+
+
+echo "copy images"
+
+  
+
+rsync -av --delete "/home/francesco/Documents/Obsidian Vault/posts" "/home/francesco/Desktop/PersonalBlog/content"
+
+  
+
+echo "copy posts"
+
+# with rsync i copy all my images and blog's posts into my hugo folders
+
+
+echo "Connecting to GH via ssh"
+
+hugo
+
+ssh -T git@github.com
+
+  
+
+echo "push changes into EmotionalData.git repo"
+
+
+git add .
+
+git commit -m "Create new post !! :D "
+
+git push
+
+  
+
+echo "everything fine and up to date"
